206998ca55146aa942a6d2c967f516620b9ef53c
Alyssa Alecci <Alyssa@Alyssas-iMac.local>
2018-07-26 23:21:41 -0400

    ....

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,15 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: "3306", //MAMP
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: "3306", //MAMP
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    });
+}
 
 module.exports = connection;
